09f9026fc9fffaff5ccf8da0d90b73a46fc5d187
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-17 00:06:48 +0300

    add useState students

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Header} from './site/Header';
 import {Body, differentCities} from './site/Body';
 import {Footer} from './site/Footer';
+import {NewComponent} from './site/NewComponent';
+
 
 function App() {
+    const [students, setStudents] = useState([
+        {id: 1, name: 'James', age: 8},
+        {id: 2, name: 'Robert', age: 38},
+        {id: 3, name: 'John', age: 28},
+        {id: 4, name: 'Michael', age: 18},
+        {id: 5, name: 'Williams', age: 28},
+        {id: 6, name: 'David', age: 58},
+        {id: 7, name: 'Richard', age: 68},
+        {id: 8, name: 'Joseph', age: 44},
+        {id: 9, name: 'Thomas', age: 33},
+        {id: 10, name: 'Charles', age: 22},
+        {id: 11, name: 'Christopher', age: 11},
+        {id: 1, name: 'James', age: 8},
+        {id: 2, name: 'Robert', age: 38},
+        {id: 3, name: 'John', age: 28},
+        {id: 4, name: 'Michael', age: 18},
+        {id: 5, name: 'Williams', age: 28},
+        {id: 6, name: 'David', age: 58},
+        {id: 7, name: 'Richard', age: 68},
+        {id: 8, name: 'Joseph', age: 44},
+        {id: 9, name: 'Thomas', age: 33},
+        {id: 10, name: 'Charles', age: 22},
+        {id: 11, name: 'Christopher', age: 11},
+    ]);
+
+
     return (
-        <>
-            <Header title={'New Header'} />
-            <Body titleForBody={'Yes'} city={differentCities}/>
-            <Footer titleForFooter={'No'}/>
-        </>
+        <NewComponent students={students}/>
     );
 }
 

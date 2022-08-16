c8ade1f730f830dcc7c3e3fa9c4ab0652b14c45d
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-16 21:06:24 +0300

    create train project

---
+++ src/site/Body.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+ type BodyPropsType = {
+    titleForBody: string
+    city : Array<ManyCityPropsType>
+}
+
+type ManyCityPropsType = {
+   id : string
+}
+
+
+export const manyCity : Array<ManyCityPropsType>  = [
+    {id: 'Kharkiv'},
+    {id: 'Kiev'},
+    {id: 'Donetsk'},
+];
+
+export const Body = (props: BodyPropsType) => {
+    return (
+        <div>
+            <h3>{props.titleForBody}</h3>
+            <h6>{props.city[2].id}</h6>
+        </div>
+    );
+}
+

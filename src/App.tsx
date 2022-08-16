f7415f11787bf570ed43462cbefd0274ed82a8f5
Serhii Yakovenko <yakovenko.sergey.work@gmail.com>
2022-08-16 21:14:23 +0300

    arguments name change

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Header} from './site/Header';
-import {Body, manyCity} from './site/Body';
+import {Body, differentCities} from './site/Body';
 import {Footer} from './site/Footer';
 
 function App() {
     return (
         <>
             <Header title={'New Header'} />
-            <Body titleForBody={'Yes'} city={manyCity}/>
+            <Body titleForBody={'Yes'} city={differentCities}/>
             <Footer titleForFooter={'No'}/>
         </>
     );
